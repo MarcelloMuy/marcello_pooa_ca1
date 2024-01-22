a5a662e5fb71248c99cca49c367210b630fb5653
Marcello Muylaert <87388600+MarcelloMuy@users.noreply.github.com>
2024-01-22 20:50:37 +0000

    Initial commit

---
+++ src/marcello_pooa_ca1/Marcello_pooa_ca1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package marcello_pooa_ca1;
+
+/**
+ *
+ * @author Marce
+ */
+public class Marcello_pooa_ca1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
