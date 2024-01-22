874db5b6d9fdb972ced299ce32efb5c09a814942
Marcello Muylaert <87388600+MarcelloMuy@users.noreply.github.com>
2024-01-22 21:22:57 +0000

    Create a File object
    Creates a file object and a basic try-catch block.

--- src/marcello_pooa_ca1/Marcello_pooa_ca1.java
+++ src/marcello_pooa_ca1/Marcello_pooa_ca1.java
@@ -4,6 +4,9 @@
  */
 package marcello_pooa_ca1;
 
+import java.io.File;
+import java.util.Scanner;
+
 /**
  *
  * @author Marce
@@ -14,7 +17,14 @@ public class Marcello_pooa_ca1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+//        Create a new 'File' object
+        File studentsList = new File ("C:\\Users\\Marce\\Desktop\\students.txt");
+//        Try-catch block, will create a new scanner object or catch an error
+        try {
+            Scanner myScanner = new Scanner(studentsList);
+        } catch (Exception e) {
+            System.out.println("Something went wrong!" + e);
+        }
     }
     
 }
